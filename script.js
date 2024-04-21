7883ec8c721154387dc5452a4a927818117627dd
EpicRaisin <gabe@muldowneyclan.com>
2024-04-20 22:13:17 -0500

    working first version

---
+++ script.js
@@ -0,0 +1,27 @@
+const hot15 = document.querySelector("#hot15")
+
+fetchList()
+
+function generateList(list) {
+    hot15.innerHTML = ""
+    list.forEach((entry, i) => {
+        let entryElement = document.createElement("div")
+        entryElement.classList.add("entry")
+
+        entryElement.innerHTML += `<div class="rank">${i + 1}</div>`
+        entryElement.innerHTML += `<img class="thumb" src="https://img.youtube.com/vi/${entry.videoId}/mqdefault.jpg"/>`
+        entryElement.innerHTML += `<table class="info"><tbody><tr><td class="title">${entry.title}</td></tr><tr><td class="author">${entry.author}<span>-</span>${entry.views} views</td></tr></tbody></table>` // crimes against humanity
+        entryElement.innerHTML += `<div class="score">${Math.round(entry.views / entry.daysOld / 10) + entry.reviewPoints}</div>`
+
+        hot15.appendChild(entryElement)
+    })
+}
+
+function fetchList(name) {
+    let req = new XMLHttpRequest()
+    req.open("GET", "/lists/1.json")
+    req.addEventListener("load", () => {
+        generateList(JSON.parse(req.responseText).list)
+    })
+    req.send()
+}
